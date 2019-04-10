083bec3684c478f11c4ab9974326e8fc41c89feb
na111 <imtanprofessional@gmail.com>
2019-04-11 02:19:58 +0700

    Update bt5.cpp

--- part-01/8-4-2019/bt5.cpp
+++ part-01/8-4-2019/bt5.cpp
@@ -4,37 +4,55 @@ using namespace std;
 
 int main()
 {
-	cout << "1.\n\n";
-	
-	for (int i = 1; i <= 5; ++i) {
-		string a(i, '*');
-		string s(5-i, ' ');
-		cout << a << s << "\n";
-	}
-	
-	cout << "\n2.\n\n";
-	
-	for (int i = 5; i >= 1; --i) {
-		string a(i, '*');
-		string s(5-i, ' ');
-		cout << a << s << "\n";
-	}
-	
-	cout << "\n3.\n\n";
-	
-	for (int i = 1; i <= 5; ++i) {
-		string s(5-i, ' ');
-		string a(i, '*');
-		cout << s << a << "\n";
-	}
-	
-	cout << "\n4.\n\n";
-	
-	for (int i = 5; i >= 1; --i) {
-		string s(5-i, ' ');
-		string a(i, '*');
-		cout << s << a << "\n";
-	}
-	
-	return 0;
+    int n;
+    cout << "Nhap so dong: ";
+    cin >> n;
+
+    cout << "1.\n\n";
+
+    for (int i = 1; i <= n; i++) {
+        string a(i, '*');
+        cout << a << "\n";
+    }
+
+    cout << "\n2.\n\n";
+
+    for (int i = n; i >= 1; i--) {
+        string a(i, '*');
+        cout << a << "\n";
+    }
+
+    cout << "\n3.\n\n";
+
+    for (int i = 1; i <= n; i++) {
+        string s(n - i, ' ');
+        string a(i, '*');
+        cout << s << a << "\n";
+    }
+
+    cout << "\n4.\n\n";
+
+    for (int i = n; i >= 1; i--) {
+        string s(n - i, ' ');
+        string a(i, '*');
+        cout << s << a << "\n";
+    }
+
+    cout << "\n5.\n\n";
+
+    for (int i = 0; i < n; i++) {
+        string s(n - i - 1, ' ');
+        string a(2 * i + 1, '*');
+        cout << s << a << "\n";
+    }
+
+    cout << "\n6.\n\n";
+
+    for (int i = n - 1; i >= 0; i--) {
+        string s(n - i - 1, ' ');
+        string a(2 * i + 1, '*');
+        cout << s << a << "\n";
+    }
+
+    return 0;
 }
