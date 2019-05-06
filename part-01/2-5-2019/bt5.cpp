77345907bd1f92a92af909a1fe7b3a68b5a62805
na111 <imtanprofessional@gmail.com>
2019-05-06 08:02:00 +0700

    Update bt5.cpp

--- part-01/2-5-2019/bt5.cpp
+++ part-01/2-5-2019/bt5.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <map>
+#include <cmath>
 
 using namespace std;
 
+/* HAM TINH NANG */
 // Ham kiem tra so hoan thien.
 bool isPerfectNumber(int value)
 {
@@ -15,6 +17,19 @@ bool isPerfectNumber(int value)
     return sum == value ? 1 : 0;
 }
 
+// Ham kiem tra so nguyen to.
+bool isPrime(int value)
+{
+    if (value < 2) return 0;
+
+    for (int i = 2; i <= sqrt(value); ++i)
+        if (!(value % i))
+            return 0;
+
+    return 1;
+}
+
+/* HAM MAIN */
 int main()
 {
     int row, col;
@@ -106,7 +121,106 @@ jump:
     cout << "4. So nguyen xuat hien nhieu nhat = " << mostFrequent << "\n";
 
     // 5. Liet ke cac dong chua gia tri nho nhat.
+    cout << "5. Cac dong chua gia tri nho nhat:\n";
     
+    for (int i = 0; i < row; ++i) {
+        loop:
+        for (int j = 0; j < col; ++j) {
+
+            if (arr[i][j] == min) {
+
+                // In hang chua gia tri nho nhat.
+                for (int k = 0; k < col; ++k)
+                    cout << arr[i][k] << (k != col - 1 ? ' ' : '\n');
+                
+                // Fix bug segmentation fault (core dumped).
+                if (i == row - 1)
+                    break;
+                else {
+                    ++i;
+                    goto loop;
+                }
+            }
+        }
+    }
+
+    // 6. Liet ke cac dong co nhieu so nguyen to nhat.
+    int maxPrimeCount = 0, currentPrimeCount = 0;
+
+    // Tim so luong so nguyen to nhieu nhat theo moi hang.
+    for (int i = 0; i < row; ++i) {
+
+        currentPrimeCount = 0;
+
+        // Dem so luong so nguyen to theo moi hang.
+        for (int j = 0; j < col; ++j)
+            if (isPrime(arr[i][j]))
+                ++currentPrimeCount;
+
+        if (maxPrimeCount < currentPrimeCount)
+            maxPrimeCount = currentPrimeCount;
+    }
+
+    if (!maxPrimeCount)
+        cout << "6. Khong co so nguyen to trong ma tran.\n";
+    else {
+        cout << "6. Cac dong co nhieu so nguyen to nhat:\n";
+
+        for (int i = 0; i < row; ++i) {
+
+            currentPrimeCount = 0;
+
+            // Dem so luong so nguyen to theo moi hang.
+            for (int j = 0; j < col; ++j)
+                if (isPrime(arr[i][j]))
+                    ++currentPrimeCount;
+
+            // In dong co nhieu so nguyen to nhat.
+            if (maxPrimeCount == currentPrimeCount)
+                for (int k = 0; k < col; ++k)
+                    cout << arr[i][k] << (k != col - 1 ? ' ' : '\n');
+        }
+    }
+
+    // 7. Hoan vi 2 dong a va b.
+    int a, b;
     
+    cout << "7. Hoan vi 2 dong a va b.\n";
+    cout << "Nhap a: "; cin >> a;
+    cout << "Nhap b: "; cin >> b;
+
+    for (int j = 0; j < col; ++j) {
+        int temp = arr[a][j];
+        arr[a][j] = arr[b][j];
+        arr[b][j] = temp;
+    }
+
+    // Xuat mang.
+    for (int i = 0; i < row; ++i)
+        for (int j = 0; j < col; ++j)
+            cout << arr[i][j] << (j != col - 1 ? ' ' : '\n');
+
+    // 8. Hoan vi 2 cot a va b.
+    cout << "8. Hoan vi 2 cot a va b.\n";
+    cout << "Nhap a: "; cin >> a;
+    cout << "Nhap b: "; cin >> b;
+
+    for (int i = 0; i < row; ++i) {
+        int temp = arr[i][a];
+        arr[i][a] = arr[i][b];
+        arr[i][b] = temp;
+    }
+
+    // Xuat mang.
+    for (int i = 0; i < row; ++i)
+        for (int j = 0; j < col; ++j)
+            cout << arr[i][j] << (j != col - 1 ? ' ' : '\n');
+
+    // Xoa cap phat dong.
+    for (int i = 0; i < row; ++i)
+        delete[] arr[i];
+    
+    delete[] arr;
+
     return 0;
 }
