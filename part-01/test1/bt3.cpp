38a7aace882fa579ecfb9cf7d09477efcb4d4aac
na111 <imtanprofessional@gmail.com>
2019-05-11 08:56:06 +0700

    Update bt3.cpp

--- part-01/test1/bt3.cpp
+++ part-01/test1/bt3.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
-#include <string>
 #include <sstream>
+#include <iomanip> // setfill, setw
 
 using namespace std;
+
+template<typename T>
+string dec_to_bin(T value)
+{
+	string binary = "";
+
+    while (value) {
+        binary = (!(value & 1) ? '0' : '1') + binary;
+        value /= 2;
+    }
+
+    return binary;
+}
 
 template<typename T>
 string dec_to_hex(T value)
 {
 	stringstream ss;
+
 	ss << "0x"
 	   << setfill('0') << setw(sizeof(T) * 2)
 	   << hex << value;
@@ -15,14 +29,17 @@ string dec_to_hex(T value)
 	return ss.str();
 }
 
-template<typename T>
-string dec_to_bin(T value)
-{
-	
-}
 int main()
 {
-	
+    int n;
+    do {
+        cout << "Nhap n (n > 0): ";
+        cin >> n;
+    } while (n <= 0);
+
+    cout << "Dec = " << n << '\n';
+    cout << "Hex = " << dec_to_hex(n) << '\n';
+    cout << "Bin = " << dec_to_bin(n) << '\n';
 	
 	return 0;
 }
