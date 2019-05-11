b09c44e627e022a971dcc4fa5d363abdc6f2e331
na111 <imtanprofessional@gmail.com>
2019-05-11 09:18:49 +0700

    Update bt3.cpp

--- part-01/test1/bt3.cpp
+++ part-01/test1/bt3.cpp
@@ -8,18 +8,18 @@ using namespace std;
 
 string dec_to_bin(int value)
 {
-	stringstream ss;
-    int i = sizeof(int) * CHAR_BIT - 1;
+        stringstream ss;
+        int i = sizeof(int) * CHAR_BIT - 1;
 
-    for (; i >= 0; --i) {
+        for (; i >= 0; --i) {
 
-        if ( i != 31 && !((i + 1) & 3) )
-            ss << " ";
+                if ( i != 31 && !((i + 1) & 3) )
+                        ss << " ";
         
-        ss << (value & (1u << i) ? "1" : "0");
-    }
+                ss << (value & (1u << i) ? "1" : "0");
+        }
 
-    return ss.str();
+        return ss.str();
 }
 
 string dec_to_hex(int value)
@@ -35,15 +35,15 @@ string dec_to_hex(int value)
 
 int main()
 {
-    int n;
-    do {
-        cout << "Nhap n (n > 0): ";
-        cin >> n;
-    } while (n <= 0);
+        int n;
+        do {
+                cout << "Nhap n (n > 0): ";
+                cin >> n;
+        } while (n <= 0);
 
-    cout << "Dec = " << n << '\n';
-    cout << "Hex = " << dec_to_hex(n) << '\n';
-    cout << "Bin = " << dec_to_bin(n) << '\n';
+        cout << "Dec = " << n << '\n';
+        cout << "Hex = " << dec_to_hex(n) << '\n';
+        cout << "Bin = " << dec_to_bin(n) << '\n';
 	
 	return 0;
 }
