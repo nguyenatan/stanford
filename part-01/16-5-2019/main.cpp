6911785edc5b4fea5fc981f3f931d195b72400f9
na111 <imtanprofessional@gmail.com>
2019-05-17 17:27:06 +0700

    Update main.cpp

--- part-01/16-5-2019/main.cpp
+++ part-01/16-5-2019/main.cpp
@@ -1,50 +1,152 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 struct thi_sinh
 {
-	int soBaoDanh;
-	string hoTen;
-	int namSinh;
-	float toan, ly, hoa;
+    int soBaoDanh;
+    string hoTen;
+    int namSinh;
+    float toan, ly, hoa;
+    float diemTrungBinh;
+    struct thi_sinh *next;
 };
 
-// 1. Nhap thong tin thi sinh.
-void input(struct thi_sinh *arr, size_t size)
+// 1.
+struct thi_sinh* input()
 {
-	for (int i = 0; i < size; ++i) {
-		cout << "Nhap so bao danh: ";
-		cin >> arr[i].soBaoDanh;
-		cout << "Nhap ho ten: ";
-		getline(cin, arr[i].hoTen);
-		cout << "Nhap nam sinh: ";
-		cin >> arr[i].namSinh;
-		cout << "Nhap diem toan, ly, hoa: ";
-		cin >> arr[i].toan;
-		cin >> arr[i].ly;
-		cin >> arr[i].hoa;
-	}
+    struct thi_sinh *node = new struct thi_sinh;
+
+    cout << "Nhap so bao danh: ";
+    cin >> node->soBaoDanh;
+
+    cout << "Nhap ho ten: ";
+    cin.ignore();
+    getline(cin, node->hoTen);
+
+    cout << "Nhap nam sinh: ";
+    cin >> node->namSinh;
+
+    cout << "Nhap diem toan, ly, hoa: ";
+    cin >> node->toan >> node->ly >> node->hoa;
+
+    node->next = NULL;
+
+    return node;
 }
 
-// 2. Lay ra top 5 thi sinh co diem cao nhat, khong co diem liet.
-// Ham tinh diem trung binh toan ly hoa.
-float diemTrungBinh(struct thi_sinh thiSinh)
+void insert(struct thi_sinh **head)
 {
-	return (thiSinh.toan + thiSinh.ly + thiSinh.hoa) / 3;
+    struct thi_sinh *node = input();
+
+    while(*head)
+        head = &(*head)->next;
+
+    *head = node;
 }
 
-struct thi_sinh* top5(struct thi_sinh *arr, size_t size)
+void display(struct thi_sinh *head)
 {
-	for (int i = 0; i < size; ++i) {
-		
-		if (diemTrungBinh)
-	}
+    if (!head) {
+        cout << "NULL\n";
+        return;
+    }
+    
+    size_t i = 1;
+    while (head) {
+        cout << setfill('0') << setw(2) << i++ << ". ";
+        cout << "So bao danh: " << head->soBaoDanh;
+        cout << "\n    Ho ten     : " << head->hoTen;
+        cout << "\n    Nam sinh   : " << head->namSinh;
+        cout << "\n    Diem toan  : " << head->toan;
+        cout << "\n    Diem ly    : " << head->ly;
+        cout << "\n    Diem hoa   : " << head->hoa;
+        cout << '\n';
+
+        head = head->next;
+    }
+}
+
+// 2.
+float diemTrungBinh(struct thi_sinh *thiSinh)
+{
+    return (thiSinh->toan + thiSinh->ly + thiSinh->hoa) / 3;
+}
+
+bool isDiemLiet(struct thi_sinh *thiSinh)
+{
+    return (thiSinh->toan < 2 || thiSinh->ly < 2 || thiSinh->hoa < 2);
 }
 
 int main()
 {
-	
-	
-	return 0;
+    size_t size;
+    cout << "Nhap so luong thi sinh: ";
+    cin >> size;
+
+    struct thi_sinh *head = input();
+
+    // 1. Nhap thong tin thi sinh.
+    size_t i = 1;
+    while (i++ < size)
+        insert(&head);
+
+    display(head);
+
+    // 2. Lay ra top 5 thi sinh co diem cao nhat, khong co diem liet.
+    struct thi_sinh *top5[5];
+    top5[0]->diemTrungBinh = -1;
+    top5[1]->diemTrungBinh = -1;
+    top5[2]->diemTrungBinh = -1;
+    top5[3]->diemTrungBinh = -1;
+    top5[4]->diemTrungBinh = -1;
+
+    while (head) {
+
+        if (!isDiemLiet(head)) {
+
+            if (diemTrungBinh(head) > top5[0]->diemTrungBinh) {
+                top5[4] = top5[3];
+                top5[3] = top5[2];
+                top5[2] = top5[1];
+                top5[1] = top5[0];
+                top5[0] = head;
+
+            } else if (diemTrungBinh(head) > top5[1]->diemTrungBinh) {
+                top5[4] = top5[3];
+                top5[3] = top5[2];
+                top5[2] = top5[1];
+                top5[1] = head;
+
+            } else if (diemTrungBinh(head) > top5[2]->diemTrungBinh) {
+                top5[4] = top5[3];
+                top5[3] = top5[2];
+                top5[2] = head;
+
+            } else if (diemTrungBinh(head) > top5[3]->diemTrungBinh) {
+                top5[4] = top5[3];
+                top5[3] = head;
+
+            } else if (diemTrungBinh(head) > top5[4]->diemTrungBinh)
+                top5[4] = head;
+        }
+
+        head = head->next;
+    }
+
+    struct thi_sinh *infoTop5 = new struct thi_sinh[5];
+
+    for (size_t i = 0; i < 5; ++i)
+        if(top5[i]->diemTrungBinh != -1) {
+            infoTop5 = top5[i];
+            infoTop5 = infoTop5->next;
+
+            if (i == 4)
+                infoTop5->next = NULL;
+        }
+
+    display(infoTop5);
+
+    return 0;
 }
